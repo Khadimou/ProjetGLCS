e672c650086f3d52a5a2db06e2d02ce2678971e8
khadimou-rassoul <khadimou-rassoul.diop@ens.uvsq.fr>
2021-01-28 12:12:18 +0100

    modification de l'affichage

--- posttreat/src/dataavg.cpp
+++ posttreat/src/dataavg.cpp
@@ -8,19 +8,20 @@ using std::cout;
 double somme=0;
 DataAvg::DataAvg( const Distributed2DField& data)
 {
-	for ( int pyy = data.distribution().extent( DY )-1; pyy >=0 ; --pyy ) {
-		for ( int yy = data.noghost_view().extent( DY )-1; yy >=0 ; --yy ) {
-			for ( int pxx = 0; pxx < data.distribution().extent( DX ); ++pxx ) {
-				if ( data.distribution().coord( DX ) == pxx && data.distribution().coord( DY ) == pyy ) {
-					for ( int xx = 0; xx < data.noghost_view().extent( DX ); ++xx ) {
-						somme += data.noghost_view(yy, xx);
-					}
-				}
-			}
-		}
-	}
-	m_data_avg=somme/(data.noghost_view().extent( DY )*data.noghost_view().extent( DX ));
-	cout << "somme"<< somme  <<"the average" << average() << "\n";
-	Write write;
-	write.write_avg(average());
+    for ( int pyy = data.distribution().extent( DY )-1; pyy >=0 ; --pyy ) {
+        for ( int yy = data.noghost_view().extent( DY )-1; yy >=0 ; --yy ) {
+            for ( int pxx = 0; pxx < data.distribution().extent( DX ); ++pxx ) {
+                if ( data.distribution().coord( DX ) == pxx && data.distribution().coord( DY ) == pyy ) {
+                    for ( int xx = 0; xx < data.noghost_view().extent( DX ); ++xx ) {
+                        somme += data.noghost_view(yy, xx);
+                    }
+                }
+            }
+        }
+    }
+    m_data_avg=somme/(data.noghost_view().extent( DY )*data.noghost_view().extent( DX ));
+    cout << " \n at t="<< data.time() << " process "<< data.distribution().rank()<< " mean data=" <<average();
+
+    Write write;
+    write.write_avg(average());
 }
